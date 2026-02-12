cb6ccfbbf9c0edac33787c672d6ae8d2573798f4
Shudi <sdbritman@gmail.comgit config --listgit config --listgit config --global user.name>
2026-02-12 11:23:14 +0200

    add init db to main

--- consumer/app/main.py
+++ consumer/app/main.py
@@ -1,8 +1,10 @@
 from kafka_consumer import ensure_topic_exists, create_consumer, consume_loop
+from db import init_db
 
 
 
 def main():
+    init_db()
     ensure_topic_exists()
     consumer = create_consumer()
     consume_loop(consumer)
