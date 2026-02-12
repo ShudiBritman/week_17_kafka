be66452b7dac3c2fa36cf9d2cdce6bdd4fa32fe4
Shudi <sdbritman@gmail.comgit config --listgit config --listgit config --global user.name>
2026-02-12 12:02:10 +0200

    fix insert func to insert many

--- consumer/app/dal.py
+++ consumer/app/dal.py
@@ -7,17 +7,21 @@ cnx = get_connection()
 
 
 
-def insert_doc(doc):
-    keys = doc.keys()
+def insert_doc(docs):
+    keys = docs[0].keys()
     columns = ", ".join(keys)
     placeholders = ", ".join(["%s"] * len(keys))
-    values = [tuple(item[k] for k in keys) for item in doc]
-    if 'customer' in values:
-        table_name = 'customers'
-    else:
-        table_name = 'orders'
-    query = f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders})"
-    cursor = cnx.exeute(query, values)
+    values = [tuple(item[k] for k in keys) for item in docs]
+    customers_values = []
+    orders_values = []
+    for row in values:
+        if 'customer' in row:
+            customers_values.append(row)
+        else:
+            orders_values.append(row)
+    query = f"INSERT INTO customers ({columns}) VALUES ({placeholders})"
+    cursor = cnx.exeutemany(query, values)
+    query = f"INSERT INTO orders ({columns}) VALUES ({placeholders})"
     cursor.commit()
     cnx.close()
 
