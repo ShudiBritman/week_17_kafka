1056ed9b55dc68ada327a08a209b3499548ac97c
Shudi <sdbritman@gmail.comgit config --listgit config --listgit config --global user.name>
2026-02-12 10:20:26 +0200

    add pull batches func

--- producer/app/dal.py
+++ producer/app/dal.py
@@ -5,3 +5,8 @@ def get_mongo_client():
     return MongoConnection().get_client()
 
 
+def pull_batches(skip):
+    coll = get_mongo_client()
+    projection = {'_id':0}
+    cursor = list(coll.find({},projection).skip(skip).limit(30))
+    return cursor
