54ac9de3cfe4dbfeeae3007d6d79949c81d4232f
Shudi <sdbritman@gmail.comgit config --listgit config --listgit config --global user.name>
2026-02-12 10:33:49 +0200

    add limit & skip to pull batch

--- producer/app/dal.py
+++ producer/app/dal.py
@@ -5,8 +5,13 @@ def get_mongo_client():
     return MongoConnection().get_client()
 
 
-def pull_batches(skip):
+def pull_batches(skip: int, limit: int):
     coll = get_mongo_client()
     projection = {'_id':0}
-    cursor = list(coll.find({},projection).skip(skip).limit(30))
-    return cursor
+    cursor = (
+        coll.find({}, projection)
+        .sort("time_insertion", 1)
+        .skip(skip)
+        .limit(limit)
+    )
+    return list(cursor)
